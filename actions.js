82ad5f663e8b0e2b85bb43fa883e809754807e7d
Maksym <maksymilian.makuch7@gmail.com>
2018-01-14 13:31:30 +0100

    Czy teraz jest dobrze?

--- actions.js
+++ actions.js
@@ -5,31 +5,7 @@ const REMOVE_COMMENT = 'REMOVE_COMMENT';
 const EDIT_COMMENT = 'EDIT_COMMENT';
 const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
 const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
-//Można tak?
-{
-	type: 'ADD_COMMENT',
-	text: 'Mój pierwszy komentarz'
-}
-{
-	type: 'REMOVE_COMMENT',
-	id: 1
-}
-{
-	type: 'EDIT_COMMENT',
-	text: 'komentarz edytowany',
-	id: 1
-}
-{
-	type: 'THUMB_UP_COMMENT',
-	id: 1,
-	thumb: 'up'
-}
-{
-	type: 'THUMB_DOWN_COMMENT',
-	id: 1,
-	thumb: 'down'
-}
-//Czy trzeba tak?
+
 function addComment(text) {
 	return {
 		type: ADD_COMMENT,
@@ -40,6 +16,7 @@ function addComment(text) {
 function removeComment(id) {
 	return {
 		type: REMOVE_COMMENT,
+		id
 	}
 }
 
@@ -47,19 +24,20 @@ function editComment(text, id) {
 	return {
 		type: EDIT_COMMENT,
 		text,
+		id
 	}
 }
 
 function thumbUpComment(id) {
 	return {
 		type: THUMB_UP_COMMENT,
+		id
 	}
 }
 
 function thumbDownComment(id) {
 	return {
 		type: THUMB_DOWN_COMMENT, 
+		id
 	}
 }
-//Czy w jedno jest zależne od drugiego?
-//Czy trzeba dorzucać np: const boundAddComment = text => dispatch(addComment(text));? Efekt jest chyba taki sam, prawda?
